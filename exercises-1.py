0f10ab5bd321b1b2a2e1bc390ec5ae73e05d26b4
Eray Alparslan <erayalparslan@yahoo.com>
2020-01-26 13:54:13 +0300

    Update exercises-1.py

--- exercises-1.py
+++ exercises-1.py
@@ -63,8 +63,29 @@ def minmax(data):
 
 mList = [45,11,56,30,90,22]
 min, max = minmax(mList)
-print('minimum is: {}'.format(min))
-print('maximum is: {}'.format(max))
+# print('minimum is: {}'.format(min))
+# print('maximum is: {}'.format(max))
+
+
+##############################################################################
+
+
+"""
+    Write a short Python function that takes a positive integer n and returns
+    the sum of the squares of all the positive integers smaller than n.
+"""
+
+
+def sumsquares(n):
+    if n <= -1:
+        print('input must be positive')
+        return
+    else:
+        return sum((x*x for x in range(1,n)))
+    
+    
+# print(sumsquares(4))
+# print(sumsquares(45))
 
 
 ##############################################################################
